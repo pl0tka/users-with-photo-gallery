b6eed75064fca252fbaadaac91ed6e75c3a04bac
plotva <huseva.a@gmail.com>
2024-02-28 13:30:12 +0100

    Add (test) useFetchAlbumsQuery in a component

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,4 +1,11 @@
+import { useFetchAlbumsQuery } from '../store';
+
 function AlbumsList({ user }) {
+  // (user) is what is passed to endpoints > fetchAlbums > query in the albumsApi.js
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+
+  console.log(data, error, isLoading);
+
   return <div>Albums for {user.name}</div>;
 }
 
