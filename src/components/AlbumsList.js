595f9d6b031cc921ee75537c16e699451fee2e08
plotva <huseva.a@gmail.com>
2024-02-29 14:21:56 +0100

    Add rendering albums list logic

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,12 +1,33 @@
 import { useFetchAlbumsQuery } from '../store';
+import Skeleton from './Skeleton';
+import ExpandablePanel from './ExpandablePanel';
+import Button from './Button';
 
 function AlbumsList({ user }) {
-  // (user) is what is passed to endpoints > fetchAlbums > query in the albumsApi.js
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
 
-  console.log(data, error, isLoading);
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} />;
+  } else if (error) {
+    content = <div>Error loading albums</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of photos in the album
+        </ExpandablePanel>
+      );
+    });
+  }
 
-  return <div>Albums for {user.name}</div>;
+  return (
+    <div>
+      <div>Albums for {user.name}</div>
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default AlbumsList;
